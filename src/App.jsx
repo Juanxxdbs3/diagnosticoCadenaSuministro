863a37187dec42ed7973e56ef1c951d2af300c81
AndrxsQ <andresquintana2004@gmail.com>
2025-06-17 00:37:19 -0500

    Añadiendo rutas para visualizar los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Encuestas from "./pages/Encuestas";
 
 function App() {
-
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/encuestas" element={<Encuestas />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
