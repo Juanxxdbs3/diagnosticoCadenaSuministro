08bb9d74174fbf938c03091d1a3171a0c7c02ffd
AndrxsQ <andresquintana2004@gmail.com>
2025-06-18 23:32:28 -0500

    Mejorand estilos de la add card

--- src/components/AddEncuestasCard.jsx
+++ src/components/AddEncuestasCard.jsx
@@ -1,7 +1,7 @@
 export function AddEncuestasCard() {
   return (
-    <div className="border border-black w-64 h-40 p-4 flex flex-col items-center justify-center text-[#8B1C1C]">
-      <h3 className="font-medium mb-2">Añadir encuesta</h3>
+    <div className="border border-[#cbd5e1] bg-white shadow-md w-64 h-40 p-4 flex flex-col items-center justify-center text-[#1e293b] rounded-lg hover:bg-[#f1f5f9] hover:shadow-lg cursor-pointer transition">
+      <h3 className="font-semibold text-lg mb-2">Añadir encuesta</h3>
       <span className="text-4xl leading-none">+</span>
     </div>
   );
