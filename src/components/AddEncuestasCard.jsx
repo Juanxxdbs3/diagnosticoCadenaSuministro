4e8640e727c1da4015cd1105165908f29cc9b2a1
AndrxsQ <andresquintana2004@gmail.com>
2025-06-18 22:55:49 -0500

    Añadiendo componente Add Encuestas Card

---
+++ src/components/AddEncuestasCard.jsx
@@ -0,0 +1,8 @@
+export function AddEncuestasCard() {
+  return (
+    <div className="border border-black w-64 h-40 p-4 flex flex-col items-center justify-center text-[#8B1C1C]">
+      <h3 className="font-medium mb-2">Añadir encuesta</h3>
+      <span className="text-4xl leading-none">+</span>
+    </div>
+  );
+}
