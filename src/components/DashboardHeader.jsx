5f498f548fc09d6d7ff1fefe86a3223d6f08d982
AndrxsQ <andresquintana2004@gmail.com>
2025-06-19 00:48:15 -0500

    Añadiendo funcionalidad a botones del header

--- src/components/DashboardHeader.jsx
+++ src/components/DashboardHeader.jsx
@@ -3,16 +3,20 @@ import { useNavigate } from "react-router-dom";
 export function DashboardHeader() {
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    navigate("/");
+  };
+
   const handleBack = () => {
-    // Aquí podrías eliminar token si usas JWT
     navigate("/encuestas");
   };
+
   return (
     <header className="w-full border-b border-[#cbd5e1] bg-white flex items-center justify-between px-6 py-4 shadow-sm">
       <h1 className="text-2xl font-bold text-[#1e293b]">Dashboard</h1>
       <div className="flex items-center gap-4">
         <button onClick={handleBack} className="text-sm text-[#1e293b] border border-[#cbd5e1] px-3 py-1 rounded hover:bg-[#f1f5f9] transition">Back</button>
-        <button className="text-sm text-[#1e293b] border border-[#cbd5e1] px-3 py-1 rounded hover:bg-[#f1f5f9] transition">Logout</button>
+        <button onClick={handleLogout} className="text-sm text-[#1e293b] border border-[#cbd5e1] px-3 py-1 rounded hover:bg-[#f1f5f9] transition">Logout</button>
       </div>
     </header>
   );
