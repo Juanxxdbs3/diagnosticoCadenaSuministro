854bd6860798a65c2629117b9b26a8eb639d0e6c
AndrxsQ <andresquintana2004@gmail.com>
2025-06-24 01:04:37 -0500

    Añadiendo ruta hacia el Dashboard al hacer click en alguna Encuesta Card

--- frontend/src/components/EncuestasCard.jsx
+++ frontend/src/components/EncuestasCard.jsx
@@ -1,7 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 export function EncuestasCard(props) {
   const { title } = props;
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate("/dashboard");
+  };
+
   return (
-    <div className="border border-[#cbd5e1] bg-white shadow-md w-64 h-40 p-4 flex flex-col justify-between rounded-lg hover:shadow-lg transition">
+    <div onClick={handleClick} className="border border-[#cbd5e1] bg-white shadow-md w-64 h-40 p-4 flex flex-col justify-between rounded-lg hover:shadow-lg transition">
       <div className="flex justify-between items-center">
         <h3 className="text-[#1e293b] font-semibold text-lg">{title}</h3>
         <span className="text-[#64748b] text-xl">...</span>
