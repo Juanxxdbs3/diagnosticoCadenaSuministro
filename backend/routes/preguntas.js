bc9f44821f883415d4052e54d90125546179b323
AndrxsQ <andresquintana2004@gmail.com>
2025-06-24 01:07:16 -0500

    Añadiendo endpoint get para las preguntas

--- backend/routes/preguntas.js
+++ backend/routes/preguntas.js
@@ -3,17 +3,28 @@ import pool from "../db.js";
 
 const router = express.Router();
 
+// Obtener todas las preguntas de la encuesta
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM preguntas ORDER BY id ASC");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error al obtener encuestas:", err);
+    res.status(500).json({ error: "Error al obtener encuestas" });
+  }
+});
+
 // Registrar nuevas preguntas
 router.post("/", async (req, res) => {
-  const { claves, encuestaId } = req.body;
+  const { encuestaId, claves} = req.body;
 
   try {
     const preguntaIds = [];
 
     for (let texto of claves) {
       const result = await pool.query(
-        "INSERT INTO preguntas (texto, encuesta_id) VALUES ($1, $2) RETURNING id",
-        [texto, encuestaId]
+        "INSERT INTO preguntas (encuesta_id, texto) VALUES ($1, $2) RETURNING id",
+        [encuestaId, texto]
       );
       preguntaIds.push(result.rows[0].id);
     }
