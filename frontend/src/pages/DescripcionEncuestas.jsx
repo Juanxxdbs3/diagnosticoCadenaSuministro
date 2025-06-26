17815954ec0017dd82a43d02c9ba58ae922c395a
AndrxsQ <andresquintana2004@gmail.com>
2025-06-26 18:17:53 -0500

    Añadiendo componente de página para la muestra y descripción de encuestas disponibles

---
+++ frontend/src/pages/DescripcionEncuestas.jsx
@@ -0,0 +1,53 @@
+import { Link } from "react-router-dom";
+import { Header } from "../components/Header";
+import { useEffect, useState } from "react";
+import { cargarEncuestas } from "../utils/utilsDb";
+
+const DescripcionEncuestas = () => {
+  // Lista de encuestas (puedes modificarla luego con datos reales)
+  const [encuestas, setEncuestas] = useState([]);
+
+  useEffect(() => {
+    const getEncuestas = async () => {
+      const data = await cargarEncuestas();
+      setEncuestas(data || []); // Previene errores si data es undefined
+    };
+  
+    getEncuestas();
+  }, []);
+  
+
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-[#f1f5f9] to-[#e2e8f0] text-[#1e293b]">
+      <Header />
+      <main className="px-6 py-10 max-w-6xl mx-auto">
+        <section className="mb-10 bg-white p-6 rounded-2xl shadow-md text-center">
+          <h2 className="text-3xl font-bold mb-4">Aviso de confidencialidad de datos</h2>
+          <p className="text-[#475569] leading-relaxed text-justify">
+          El Grupo de Investigación Gestión y Desarrollo Empresarial de la Facultad de Ciencias Económicas, Administrativas y Contables de la Fundación universitaria Tecnológico Comfenalco, extiende invitación para diligenciar la siguiente encuesta voluntaria y anónima.
+          <br /><br />
+          El presente estudio de investigación tiene como objetivo: Realizar un diagnóstico de la Administración de la Cadena de Suministro (SCM) en empresas Cartageneras a través de un trabajo de campo que permita la formulación de estrategias para el mejoramiento continuo de las organizaciones.
+          <br /><br />
+          Se garantiza total anonimato y confidencialidad y de antemano agradecemos el tiempo empleado en responder esta encuesta.
+          </p>
+        </section>
+
+        <section>
+          <h3 className="text-2xl font-semibold mb-6">Listado de Encuestas</h3>
+          <div className="grid md:grid-cols-2 gap-6">
+            {encuestas.map((encuesta) => (
+              <Link to={`/responder/${encuesta.id}`} key={encuesta.id}>
+                <div className="bg-white rounded-2xl shadow-md p-5 hover:shadow-lg transition cursor-pointer">
+                  <h4 className="text-xl font-bold mb-1">{encuesta.titulo}</h4>
+                  <p className="text-sm text-[#94a3b8]">Preguntas: {encuesta.id}</p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default DescripcionEncuestas;
