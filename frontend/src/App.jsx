7583cb5802071bf849abc478311e11d38f06f849
AndrxsQ <andresquintana2004@gmail.com>
2025-06-26 18:14:00 -0500

    Añadiendo nuevas rutas para incluir páginas y funcionalidad de la respuesta de encuestas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,17 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Encuestas from "./pages/Encuestas";
 import Registro from "./pages/Registro";
+import Landing from "./pages/Landing";
+import DescripcionEncuestas from "./pages/DescripcionEncuestas";
+import ResponderEncuesta from "./pages/ResponderEncuesta";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/descripcionEncuestas" element={<DescripcionEncuestas />} />
+      <Route path="/responder/:id" element={<ResponderEncuesta />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/registro" element={<Registro />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/encuestas" element={<Encuestas />} />
